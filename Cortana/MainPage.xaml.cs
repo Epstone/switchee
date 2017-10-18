d65542dbb52b471980f4e10121a03bbc604f83fe
Patrick Epstein <Patrick.Epstein@zuehlke.com>
2017-10-18 18:13:47 +0200

    automated code cleanup

--- Cortana/MainPage.xaml.cs
+++ Cortana/MainPage.xaml.cs
@@ -1,47 +1,38 @@
-﻿using CortanaHomeAutomation.MainApp.Utility;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Net;
-using System.Threading.Tasks;
-using Windows.ApplicationModel.Resources;
-using Windows.Foundation;
-using Windows.UI.Popups;
-using Windows.UI.ViewManagement;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
-namespace CortanaHomeAutomation.MainApp
+﻿namespace CortanaHomeAutomation.MainApp
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Net;
+    using Windows.ApplicationModel.Resources;
+    using Windows.Foundation;
+    using Windows.UI.Popups;
+    using Windows.UI.ViewManagement;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Navigation;
+    using Utility;
+
+    public sealed partial class MainPage
     {
         private AppState _state;
-        ResourceLoader stringRessource = new Windows.ApplicationModel.Resources.ResourceLoader();
-        
+        private readonly ResourceLoader stringRessource = new ResourceLoader();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ApplicationView.PreferredLaunchViewSize = new Size {Height = 550, Width = 500};
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
+
         public bool ShowDeletionButtons { get; set; }
 
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            var state = (AppState)e.Parameter;
-            this.UpdateViewFromState(state);
+            var state = (AppState) e.Parameter;
+            UpdateViewFromState(state);
             base.OnNavigatedTo(e);
-        }
-
-        public MainPage()
-        {
-            this.InitializeComponent();
-
-            ApplicationView.PreferredLaunchViewSize = new Size { Height = 550, Width = 500 };
-            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
         }
 
         private static ObservableCollection<Device> CreateDevices()
@@ -66,7 +57,6 @@ namespace CortanaHomeAutomation.MainApp
             IPAddress outValue;
             if (!IPAddress.TryParse(_state.GatewayIPAddress, out outValue))
             {
-                
                 ShowUserWarning(stringRessource.GetString("WarningIncorrectIPAddress"));
                 return;
             }
@@ -107,25 +97,21 @@ namespace CortanaHomeAutomation.MainApp
 
             if (dialogResult == ContentDialogResult.Primary)
             {
-                this._state.Devices.Add(dialog.ViewModel.Device);
+                _state.Devices.Add(dialog.ViewModel.Device);
                 await Startup.StoreAppState(_state);
-            }
-            else
-            {
-                // User pressed Cancel or the back arrow.
             }
         }
 
         private void btn_off_OnClick(object sender, RoutedEventArgs e)
         {
-            this.lv_Devices.SelectedIndex = -1;
+            lv_Devices.SelectedIndex = -1;
             var device = GetDeviceFromEvent(e);
             ExecuteCommand(device, false);
         }
 
         private void btn_on_OnClick(object sender, RoutedEventArgs e)
         {
-            this.lv_Devices.SelectedIndex = -1;
+            lv_Devices.SelectedIndex = -1;
             var device = GetDeviceFromEvent(e);
             ExecuteCommand(device, true);
         }
@@ -134,7 +120,7 @@ namespace CortanaHomeAutomation.MainApp
         private void btn_deleteDevice_OnClick(object sender, RoutedEventArgs e)
         {
             var device = GetSelectedDevice();
-            this._state.Devices.Remove(device);
+            _state.Devices.Remove(device);
         }
 
         private Device GetDeviceFromEvent(RoutedEventArgs e)
@@ -159,59 +145,56 @@ namespace CortanaHomeAutomation.MainApp
 
             if (dialogResult == ContentDialogResult.Primary)
             {
-                var index = this._state.Devices.IndexOf(selectedDevice);
-                this._state.Devices.Remove(selectedDevice);
-                this._state.Devices.Insert(index, clonedDeviceForEdits);
+                var index = _state.Devices.IndexOf(selectedDevice);
+                _state.Devices.Remove(selectedDevice);
+                _state.Devices.Insert(index, clonedDeviceForEdits);
 
                 await Startup.StoreAppState(_state);
-            }
-            else
-            {
-                // User pressed Cancel or the back arrow, so do nothing
             }
         }
 
         private Intertechno GetSelectedDevice()
         {
-            return this.lv_Devices.SelectedItem as Intertechno;
+            return lv_Devices.SelectedItem as Intertechno;
         }
 
         private async void btn_saveConfig_Click(object sender, RoutedEventArgs e)
         {
-            await Startup.SaveAppStateToUserDefinedLocation(this._state);
+            await Startup.SaveAppStateToUserDefinedLocation(_state);
         }
 
         private async void btn_loadConfig_Click(object sender, RoutedEventArgs e)
         {
             var state = await Startup.LoadAppStateFromUserDefinedLocation();
-            this.UpdateViewFromState(state);
+            UpdateViewFromState(state);
         }
 
         private void UpdateViewFromState(AppState state)
         {
-            this._state = state;
-            this.lv_Devices.ItemsSource = _state.Devices;
+            _state = state;
+            lv_Devices.ItemsSource = _state.Devices;
 
             if (string.IsNullOrEmpty(_state.GatewayIPAddress))
             {
-                this.btn_settings_Click(this, null);
+                ShowSettingsDialog();
             }
+        }
+
+        private void ShowSettingsDialog()
+        {
+            btn_settings_Click(this, null);
         }
 
         private async void btn_settings_Click(object sender, RoutedEventArgs e)
         {
-            SettingsDialog settingsDialog = new SettingsDialog(this._state.GatewayIPAddress, this._state.GatewayPort);
+            SettingsDialog settingsDialog = new SettingsDialog(_state.GatewayIPAddress, _state.GatewayPort);
             ContentDialogResult dialogResult = await settingsDialog.ShowAsync();
 
             if (dialogResult == ContentDialogResult.Primary)
             {
-                this._state.GatewayIPAddress = settingsDialog.GatewayIPAddress.Value;
-                this._state.GatewayPort = int.Parse(settingsDialog.GatewayPort.Value);
+                _state.GatewayIPAddress = settingsDialog.GatewayIPAddress.Value;
+                _state.GatewayPort = int.Parse(settingsDialog.GatewayPort.Value);
                 await Startup.StoreAppState(_state);
-            }
-            else
-            {
-                // User pressed Cancel or the back arrow.
             }
         }
 
@@ -226,81 +209,80 @@ namespace CortanaHomeAutomation.MainApp
     {
         public SampleDeviceData()
         {
-            this.Add(new Intertechno()
+            Add(new Intertechno
             {
                 UserDefinedName = "Schlafzimmer Fenster",
                 Masterdip = "A",
                 Slavedip = "5"
             });
 
-            this.Add(new Intertechno()
+            Add(new Intertechno
             {
                 UserDefinedName = "Wohnzimmerbeleuchtung",
                 Masterdip = "A",
                 Slavedip = "7"
             });
 
-            this.Add(new Intertechno()
+            Add(new Intertechno
             {
                 UserDefinedName = "Rollo Küche",
                 Masterdip = "A",
                 Slavedip = "12"
             });
 
-            this.Add(new Intertechno()
+            Add(new Intertechno
             {
                 UserDefinedName = "Rollo Küche",
                 Masterdip = "A",
                 Slavedip = "12"
             });
 
-            this.Add(new Intertechno()
+            Add(new Intertechno
             {
                 UserDefinedName = "Rollo Küche",
                 Masterdip = "A",
                 Slavedip = "12"
             });
 
-            this.Add(new Intertechno()
+            Add(new Intertechno
             {
                 UserDefinedName = "Rollo Küche",
                 Masterdip = "A",
                 Slavedip = "12"
             });
 
-            this.Add(new Intertechno()
+            Add(new Intertechno
             {
                 UserDefinedName = "Rollo Küche",
                 Masterdip = "A",
                 Slavedip = "12"
             });
 
-            this.Add(new Intertechno()
+            Add(new Intertechno
             {
                 UserDefinedName = "Rollo Küche",
                 Masterdip = "A",
                 Slavedip = "12"
             });
-            this.Add(new Intertechno()
-            {
-                UserDefinedName = "Rollo Küche",
-                Masterdip = "A",
-                Slavedip = "12"
-            });
-
-            this.Add(new Intertechno()
-            {
-                UserDefinedName = "Rollo Küche",
-                Masterdip = "A",
-                Slavedip = "12"
-            });
-            this.Add(new Intertechno()
+            Add(new Intertechno
             {
                 UserDefinedName = "Rollo Küche",
                 Masterdip = "A",
                 Slavedip = "12"
             });
 
+            Add(new Intertechno
+            {
+                UserDefinedName = "Rollo Küche",
+                Masterdip = "A",
+                Slavedip = "12"
+            });
+            Add(new Intertechno
+            {
+                UserDefinedName = "Rollo Küche",
+                Masterdip = "A",
+                Slavedip = "12"
+            });
         }
     }
 }
